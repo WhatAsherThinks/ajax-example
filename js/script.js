b64514aaa66ea059ae38a151d98947e2a7bf63ef
Ashley DeMadet <darthvader@ashleys-mbp.lab.lagcc>
2017-11-27 19:54:02 -0500

    creates dynamic search

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-fetch("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5")
+var input = document.querySelector('#user-gif-search');
+var searchBtn = document.querySelector('#submit-gif-search');
+var gifContainer = document.querySelector('#gif-result-container');
+
+
+function findGifs(userInput){
+fetch("https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=dc6zaTOxFJmzC&limit=5")
 .then(function(response) {
   return response.json();
   })
   .then(function(response) {
-    showGifs(response.data)
+    showGifs(response.data);
   });
+}
+
 
 function showGifs(data){
+  gifContainer.innerHTML = '';
   for (var i = 0; i < data.length; i++) {
     var img = document.createElement('img');
     img.src = data[i].images.original.url;
-    document.body.appendChild(img);
+    gifContainer.appendChild(img);
   }
+}
+
+if (input && searchBtn && gifContainer) {
+  searchBtn.addEventListener('click', function(){
+    var userInput = input.value;
+    if(userInput !==''){
+      findGifs(userInput);
+    }
+
+  })
 }
